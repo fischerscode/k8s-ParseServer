fad6908654b100568c97b9f9371524568296230b
maaeps <github@maaeps.de>
2020-02-15 17:27:48 +0100

    cloudcode to cm

---
+++ cloudcode/main.js
@@ -0,0 +1,1 @@
+//cloudcode main file
--- generatecm.sh
+++ generatecm.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 kubectl create configmap parse-dashboard-config --from-file=dashboard.json
 kubectl create configmap parse-config --from-env-file=server.env
+kubectl create configmap parse-cloudcode --from-file=cloudcode/
